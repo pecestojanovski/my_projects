94e713c0d197b334ed8d719e07f908c306218f1e
Pece Stojanovski <31314085+pecestojanovski@users.noreply.github.com>
2017-12-17 20:07:25 +0100

    Add files via upload

---
+++ hangman/js/wordlibrary.js
@@ -0,0 +1,48 @@
+Words = {};
+Words.List = [];
+
+Words.List[0] = "lightbulb";
+Words.List[1] = "album";
+Words.List[2] = "paint";
+Words.List[3] = "blood";
+Words.List[4] = "neighbour";
+Words.List[5] = "revenge";
+Words.List[6] = "queen";
+Words.List[7] = "bride";
+Words.List[8] = "grammar";
+Words.List[9] = "ghost";
+
+Words.List[10] = "computer";
+Words.List[11] = "mouse";
+Words.List[12] = "keyboard";
+Words.List[13] = "screen";
+Words.List[14] = "speaker";
+Words.List[15] = "display";
+Words.List[16] = "laptop";
+Words.List[17] = "guitar";
+Words.List[18] = "amplifier";
+Words.List[19] = "headphones";
+
+Words.List[20] = "coyote";
+Words.List[21] = "tiger";
+Words.List[22] = "shark";
+Words.List[23] = "snake";
+Words.List[24] = "spider";
+Words.List[25] = "pigeon";
+Words.List[26] = "penguin";
+Words.List[27] = "turtle";
+Words.List[28] = "elephant";
+Words.List[29] = "zebra";
+
+Words.List[30] = "heart";
+Words.List[31] = "jeans";
+Words.List[32] = "forest";
+Words.List[33] = "prison";
+Words.List[34] = "viking";
+Words.List[35] = "whisper";
+Words.List[36] = "anarchy";
+Words.List[37] = "detective";
+Words.List[38] = "monster";
+Words.List[39] = "paradise";
+
+Words.Length = Words.List.length;
